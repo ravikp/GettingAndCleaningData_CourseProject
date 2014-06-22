cbd82d4d088b370d4468da60b5c76a3b35b415ad
Ravi Kumar Pasumarthy <ravi.pasumarthy@gmail.com>
2014-06-22 19:22:55 +0200

    Removing observation_id from subject

--- run_analysis.R
+++ run_analysis.R
@@ -6,8 +6,9 @@ load_subject_data <- function(dataset_type){
       subject_file_name = paste("subject_", dataset_type, ".txt", sep="")
       subject_file = file.path(current_working_directory, "Dataset", dataset_type, subject_file_name)
       subject_df = read.csv(subject_file, col.names=c("subject_id"), colClasses=c("numeric"), header=FALSE)
-      observation_df = data.frame(observation_id = seq_along(subject_df$subject_id))      
-      return(cbind(observation_df, subject_df))
+      # observation_df = data.frame(observation_id = seq_along(subject_df$subject_id))      
+      # return(cbind(observation_df, subject_df))
+      return(subject_df)
     }
     else
       stop(paste("cannot find dataset_type:",dataset_type,". Recognized dataset_types can be either test or train", sep=""))
